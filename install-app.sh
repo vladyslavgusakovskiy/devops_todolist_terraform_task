8cdcafa4aace2a287f1ea73ff8070799578dcaff
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-07-04 21:33:34 +0300

    updated install-app.sh

--- install-app.sh
+++ install-app.sh
@@ -14,11 +14,13 @@ apt-get install python3-pip -yq
 mkdir /app 
 # make sure to uncomment the line bellow and update the link with your GitHub username
 git clone https://github.com/vladyslavgusakovskiy/azure_task_12_deploy_app_with_vm_extention.git
-sudo cp -r devops_todolist_terraform_task/app/* /app
+cp -r azure_task_12_deploy_app_with_vm_extention/app/* /app
+
+
+chmod +x /app/start.sh
 
 # create a service for the app via systemctl and start the app
 mv /app/todoapp.service /etc/systemd/system/
-sudo chmod 777 /app/start.sh
 systemctl daemon-reload
 systemctl start todoapp
 systemctl enable todoapp
